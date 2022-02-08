459557941b4a4180645d8c1d6bbd10a70a457782
limacwin <mgkamat511@gmail.com>
2022-02-09 02:14:59 +0530

    Added restrictions to password length and changed the default value to 8

--- javascript.js
+++ javascript.js
@@ -2,14 +2,17 @@ function getPassword() {
     var charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+?><:{}[]";
     var password_length = document.getElementById("password_length").value;
     if (password_length == "")
-        password_length = 16;
-    var password = "";
+        password_length = 8;
+    else if (password_length < "8" || password_length > "128") {
+        password_length = 8;
+        alert("Password length too long, resetting to default. Enter a value between 8 and 128.");
+    }
 
+    var password = "";
     for (var i = 0; i < password_length; i++) {
         var random_number = Math.floor(Math.random() * charset.length);
         password += charset.substring(random_number, random_number + 1);
     }
-
     document.getElementById("password").value = password;
 }
 
